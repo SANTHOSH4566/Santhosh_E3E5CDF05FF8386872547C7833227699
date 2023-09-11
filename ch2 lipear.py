6475745c97b9118c061467fb1b36d7e8d9e65fbe
SANTHOSH4566 <144442132+SANTHOSH4566@users.noreply.github.com>
2023-09-11 14:25:12 +0530

    Add files via upload

---
+++ ch2 lipear.py
@@ -0,0 +1,15 @@
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+# Input the number
+number = int(input("Enter a number: "))
+
+# Calculate the factorial using the recursive function
+result = factorial(number)
+
+# Print the result
+print(f"The factorial of {number} is {r luesult}")
